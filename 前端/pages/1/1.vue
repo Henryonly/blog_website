dc0600a357991a10106aed227f82316bf60a0b81
Henryonly <1746023370@qq.com>
2025-08-21 15:14:41 +0800

    front is ok

---
+++ 前端/pages/1/1.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		hi
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				'hhhh'
+			}
+		},
+		methods: {
+			'aaaa'
+		}
+	}
+</script>
+
+<style>
+
+</style>
